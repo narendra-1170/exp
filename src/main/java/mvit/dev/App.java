b5fe84e569662d83513c4db5f931c53da262f6ee
Maulik95lab <maulikforzaplayer@gmail.com>
2025-06-09 11:43:58 +0530

    first commit

---
+++ src/main/java/mvit/dev/App.java
@@ -0,0 +1,13 @@
+package mvit.dev;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
